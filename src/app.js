a403b36287d49110614208b5922124aacdd6473a
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-23 01:45:52 -0400

    updating files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
-"use strict";
+
+
+
+
+
+
+
+
+
+function onMapClick(e) {
+    alert("You clicked the map at " + e.latlng);
+}
+
+map.on('click', onMapClick);
+
+
+
+
+
+
+
+
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+var marker = L.marker([51.5, -0.09]).addTo(map);
 console.log("typescript is working");
