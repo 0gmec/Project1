bcc06f816de7f48385bd34c0c7779f1a9eda29c7
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-07-25 23:24:22 -0400

    update html and css

--- src/app.ts
+++ src/app.ts
@@ -5,4 +5,4 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-console.log("typescript is working")
+console.log("typescript is working!")
